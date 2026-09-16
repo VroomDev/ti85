d8ad85a3ff058f068463bd80188826ecea7d4b55
Chris Busch <cgbusch@yahoo.com>
2026-09-15 20:57:52 -0500

    fixed LVL files

--- vic20caves/build.bat
+++ vic20caves/build.bat
@@ -25,6 +25,7 @@ if errorlevel 1 exit /b 1
 echo Compiling for VIC-20 +32K...
 
 del header.s.*.o 2>nul
+del "%PRGDIR%\header.s.*.o" 2>nul
 
 cl65 -O -t vic20 -C "%~dp0vic20-map.cfg" -o "%PRGDIR%\%PRGNAME%" header.s main.c
 rem    cl65 -O -t vic20 -C vic20-32k.cfg -o mapview.prg header.s main.c
